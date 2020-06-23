18bd742099e6e22d32815acb6521132f60174a50
6ALLIKA <skorik009@gmail.com>
2020-06-23 17:48:19 +0300

    Implement the simple Spring Boot

---
+++ src/main/java/com/ma/springboot/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.ma.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping
+    public String getIndex() {
+        return "index";
+    }
+}
