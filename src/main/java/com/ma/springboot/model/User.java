e44882fb59786df324c8e06fb01b76dd988be20b
6ALLIKA <skorik009@gmail.com>
2020-07-30 00:46:49 +0300

    test

--- src/main/java/com/ma/springboot/model/User.java
+++ src/main/java/com/ma/springboot/model/User.java
@@ -2,6 +2,7 @@ package com.ma.springboot.model;
 
 import java.util.Set;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import lombok.Data;
@@ -16,7 +17,7 @@ public class User {
     @NonNull
     private String id;
     private String profileName;
-    @OneToMany(mappedBy = "user")
+    @OneToMany(mappedBy = "user", fetch = FetchType.EAGER)
     private Set<Review> reviews;
 
     public User(String profileName) {
